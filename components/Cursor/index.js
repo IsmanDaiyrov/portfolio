ddd126e48214236822579919f728046948af5698
Isman Daiyrov <idaiyrov@gmail.com>
2023-04-19 16:39:42 -0500

    updated cursor movement speed

--- components/Cursor/index.js
+++ components/Cursor/index.js
@@ -27,7 +27,7 @@ const Cursor = () => {
           dimensions={30}
           fill={getCusomColor()}
           smoothness={{
-            movement: 0.3,
+            movement: 0.7,
             scale: 0.1,
             opacity: 0.2,
           }}
